9d91522ede96031ababb5fb167281a9d269aa031
Overjoy06-Github <56468229+Overjoy06-Github@users.noreply.github.com>
2024-04-20 22:16:18 +0800

    Update iot-sensors.ino
    Added Temperature Sensor

--- iot-sensors/iot-sensors.ino
+++ iot-sensors/iot-sensors.ino
@@ -1,17 +1,37 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
 const int pH_pin = A0;
 
 void setup() {
   Serial.begin(9600);
+  sensors.begin();
 }
 
 void loop() {
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempCByIndex(0);
+
   int rawValue = analogRead(pH_pin);
   
   float pH_value = analogToPH(rawValue);
   
   //Serial.print("pH value: ");
-  Serial.println(pH_value);
-
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+    //Serial.print("Temperature: ");
+    Serial.print(pH_value);
+    Serial.print(" ");
+    Serial.print(tempC);
+  } 
+  else
+  {
+    Serial.println("Undefined");
+  }
   delay(1000); 
 }
 
