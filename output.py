38558f507c68b43ae4ea0b14c8ee91bc4684cbab
Overjoy06-Github <56468229+Overjoy06-Github@users.noreply.github.com>
2024-04-20 23:39:20 +0800

    Update output.py
    Fixed emptied .txt file gap

--- output.py
+++ output.py
@@ -4,11 +4,14 @@ import time
 ser = serial.Serial('COM3', 9600)  
 while True:
     try:
-        f = open('output.txt', 'w')
         line = ser.readline().decode().strip()
-        f.write(line + '\n')
-        print(line)
-        f.close()
-        time.sleep(1)
+        if line:
+            f = open('output.txt', 'w')
+            f.write(line + '\n')
+            print(line)
+            f.close()
+            time.sleep(1)
+        else:
+            time.sleep(1)
     except Exception as e:
         print(e)
