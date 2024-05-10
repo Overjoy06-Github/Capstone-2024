53645b482c12c9793b668196f23f859320293b99
Overjoy06-Github <56468229+Overjoy06-Github@users.noreply.github.com>
2024-05-10 22:16:21 +0800

    Update iot-sensors.ino

--- iot-sensors/iot-sensors.ino
+++ iot-sensors/iot-sensors.ino
@@ -3,13 +3,11 @@
 #include <runningAvg.h>
 
 #define ONE_WIRE_BUS 2
-#define calibrationOffset 0.00
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 const int pH_pin = A0;
-
 runningAvg smoother(10); 
 
 void setup() {
@@ -24,21 +22,15 @@ void loop() {
   float pH = analogToPH(analogRead(pH_pin));
   float ave_pH = smoother.addData(pH);
 
-  Serial.print("pH: ");
-  Serial.println(pH);
-  Serial.print(" Average pH: ");
-  Serial.println(ave_pH);
+  Serial.print(ave_pH);
+  Serial.print(" ");
 
-  //Serial.print("Temperature: ");
-  //Serial.println(tempC);
-
+  Serial.println(tempC);
   delay(1000);
 }
 
 float analogToPH(int raw) {
-  float voltage = raw * (5.0 / 1023.0);
-  voltage = voltage - calibrationOffset;
+  float voltage = raw * (5.0 / 1023.0 );
   float pH = voltage * (14.0 / 5.0);
-
   return pH;
 }
